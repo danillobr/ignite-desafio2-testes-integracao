ee599161cfd73b0ec052ba87c0bf4b06c17196ba
danillobr <danillorodriguesabreu@gmail.com>
2023-01-05 12:15:34 -0300

    Teste unitário de CreateStatementController

--- src/modules/statements/useCases/createStatement/CreateStatementController.spec.ts
+++ src/modules/statements/useCases/createStatement/CreateStatementController.spec.ts
@@ -7,7 +7,7 @@ import createConnection from "../../../../database";
 
 let connection: Connection;
 
-describe("Get balance", () => {
+describe("Create statement", () => {
   beforeAll(async () => {
     connection = await createConnection();
     await connection.runMigrations();
@@ -28,7 +28,7 @@ describe("Get balance", () => {
     await connection.close();
   });
 
-  it("Should be able to get account balance", async () => {
+  it("Should be able to create a deposit statement", async () => {
     const responseToken = await request(app).post("/api/v1/sessions").send({
       email: "danillo@email.com",
       password: "danilo123",
@@ -37,13 +37,13 @@ describe("Get balance", () => {
     const { token } = responseToken.body;
 
     const response = await request(app)
-      .get("/api/v1/statements/balance")
+      .post("/api/v1/statements/deposit")
+      .send({ amount: 100, description: "Depositing 100 R$" })
       .set({ Authorization: `Bearer ${token}` });
 
-    expect(response.status).toBe(200);
-    expect(response.body).toHaveProperty("balance");
-    expect(response.body).toHaveProperty("statement");
-    expect(response.body.balance === 0);
-    expect(response.body.statement.length).toBe(0);
+    expect(response.status).toBe(201);
+    expect(response.body).toHaveProperty("id");
+    expect(response.body.amount).toEqual(100);
+    expect(response.body.type).toEqual("deposit");
   });
 });
